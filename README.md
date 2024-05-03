a6ade08c64987a4e8c4c8bb1715ba3b1f6c9b21f
Scott Mackenzie <scott@standingby.co>
2024-05-03 12:38:06 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# The New Daily - Scheduled Posts Worker
+
+Run locally
+
+```
+npx wrangler dev --test-scheduled
+```
+
+```
+curl "http://localhost:8787/__scheduled?cron=*+*+*+*+*"
+```
---
+++ src/index.js
@@ -0,0 +1,26 @@
+/**
+ * Welcome to Cloudflare Workers!
+ *
+ * This is a template for a Scheduled Worker: a Worker that can run on a
+ * configurable interval:
+ * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+	async scheduled(event, env, ctx) {
+		const timestamp = +new Date();
+		const url = `${env.WORDPRESSS_DOMAIN}/wp-json/tnd/v1/scheduled-posts?t=${timestamp}`;
+		const res = await fetch(url, {
+			method: 'GET',
+		});
+		const data = await res.json();
+		data.timestampVercel = timestamp;
+		console.log(data);
+	},
+};
